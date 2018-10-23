9d12fc557e03d3a0640ad6e8366cd9a4d2a8587d
Andrew Tran <tran.andrew.a@gmail.com>
2018-10-23 15:00:57 -0400

    Added land gen modularization

--- scripts/src/init.js
+++ scripts/src/init.js
@@ -1,66 +1,67 @@
-var game = new Phaser.Game(800, 400, Phaser.AUTO, 'test', null, true, false);
-landGenerator = new LandGeneration();
+window.onload = function() {
 
-var Game = function (game) { };
-
-Game.Boot = function (game) { };
-
-var isoGroup, cursorPos, cursor;
-var landGenerator;
-
-Game.Boot.prototype =
-{
-    preload: function () {
-        game.load.image('tile', '../assets/tiles/KaelanTest.png');
-
-        game.time.advancedTiming = true;
-
-        // Add and enable the plug-in.
-        game.plugins.add(new Phaser.Plugin.Isometric(game,0));
-
-        // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
-        // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
-        game.iso.anchor.setTo(0.5, 0.2);
-    },
-    create: function () {
-        // Create a group for our tiles.
-        isoGroup = game.add.group();
-
-        // Let's make a load of tiles on a grid.
-        landGenerator.spawnTiles();
-        
-
-        // Provide a 3D position for the cursor
-        cursorPos = new Phaser.Plugin.Isometric.Point3();
-    },
-    update: function () {
-        // Update the cursor position.
-        // It's important to understand that screen-to-isometric projection means you have to specify a z position manually, as this cannot be easily
-        // determined from the 2D pointer position without extra trickery. By default, the z position is 0 if not set.
-        game.iso.unproject(game.input.activePointer.position, cursorPos);
-
-        // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
-        isoGroup.forEach(function (tile) {
-            var inBounds = tile.isoBounds.containsXY(cursorPos.x, cursorPos.y);
-            // If it does, do a little animation and tint change.
-            if (!tile.selected && inBounds) {
-                tile.selected = true;
-                tile.tint = 0x86bfda;
-                game.add.tween(tile).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
-            }
-            // If not, revert back to how it was.
-            else if (tile.selected && !inBounds) {
-                tile.selected = false;
-                tile.tint = 0xffffff;
-                game.add.tween(tile).to({ isoZ: 0 }, 200, Phaser.Easing.Quadratic.InOut, true);
-            }
-        });
-    },
-    render: function () {
-        game.debug.text("I love Kaelan");
-
-    },
-};
-
-game.state.add('Boot', Game.Boot);
-game.state.start('Boot');
+    var game = new Phaser.Game(800, 400, Phaser.AUTO, 'test', null, true, false);
+    
+    var Game = function (game) { };
+    
+    Game.Boot = function (game) { };
+    
+    var isoGroup, cursorPos, cursor;
+    
+    
+    Game.Boot.prototype =
+    {
+        preload: function () {
+            game.load.image('tile', '../assets/tiles/KaelanTest.png');
+    
+            game.time.advancedTiming = true;
+    
+            // Add and enable the plug-in.
+            game.plugins.add(new Phaser.Plugin.Isometric(game,0));
+    
+            // This is used to set a game canvas-based offset for the 0, 0, 0 isometric coordinate - by default
+            // this point would be at screen coordinates 0, 0 (top left) which is usually undesirable.
+            game.iso.anchor.setTo(0.5, 0.2);
+        },
+        create: function () {
+            //Instantiate land generator
+            var landGenerator = new LandGeneration(game);
+            // Let's make a load of tiles on a grid.
+            landGenerator.drawTiles();
+            isoGroup = landGenerator.isoGroup;
+    
+            // Provide a 3D position for the cursor
+            cursorPos = new Phaser.Plugin.Isometric.Point3();
+        },
+        update: function () {
+            // Update the cursor position.
+            // It's important to understand that screen-to-isometric projection means you have to specify a z position manually, as this cannot be easily
+            // determined from the 2D pointer position without extra trickery. By default, the z position is 0 if not set.
+            game.iso.unproject(game.input.activePointer.position, cursorPos);
+    
+            // Loop through all tiles and test to see if the 3D position from above intersects with the automatically generated IsoSprite tile bounds.
+            isoGroup.forEach(function (tile) {
+                var inBounds = tile.isoBounds.containsXY(cursorPos.x, cursorPos.y);
+                // If it does, do a little animation and tint change.
+                if (!tile.selected && inBounds) {
+                    tile.selected = true;
+                    tile.tint = 0x86bfda;
+                    game.add.tween(tile).to({ isoZ: 4 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                }
+                // If not, revert back to how it was.
+                else if (tile.selected && !inBounds) {
+                    tile.selected = false;
+                    tile.tint = 0xffffff;
+                    game.add.tween(tile).to({ isoZ: 0 }, 200, Phaser.Easing.Quadratic.InOut, true);
+                }
+            });
+        },
+        render: function () {
+            game.debug.text("I love Kaelan");
+    
+        },
+    };
+    
+    game.state.add('Boot', Game.Boot);
+    game.state.start('Boot');
+}
--- scripts/src/landGeneration.js
+++ scripts/src/landGeneration.js
@@ -1,18 +1,19 @@
-var LandGeneration = function () {
-    this.width = 8;
-    this.height = 8;
+var LandGeneration = function (game) {
+    this.game = game;
+
+    // Create a group for our tiles.
+    this.isoGroup = this.game.add.group();
 }
 
-LandGeneration.protoype = {
-    spawnTiles: function () {
-        var tile;
-        for (var xx = 0; xx < 289; xx += 36) {
-            for (var yy = 0; yy < 256; yy += 36) {
-                // Create a tile using the new game.add.isoSprite factory method at the specified position.
-                // The last parameter is the group you want to add it to (just like game.add.sprite)
-                tile = game.add.isoSprite(xx, yy, 0, 'tile', 0, isoGroup);
-                tile.anchor.set(0.5, 0);
-            }
+
+LandGeneration.prototype.drawTiles = function () {
+    var tile;
+    for (var xx = 0; xx < 289; xx += 36) {
+        for (var yy = 0; yy < 256; yy += 36) {
+            // Create a tile using the new game.add.isoSprite factory method at the specified position.
+            // The last parameter is the group you want to add it to (just like game.add.sprite)
+            tile = this.game.add.isoSprite(xx, yy, 0, 'tile', 0, this.isoGroup);
+            tile.anchor.set(0.5, 0);
         }
     }
 }
