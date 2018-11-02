a42a7852f11f27f36379ca3cf00239ed7ac3d67a
Andrew Tran <tran.andrew.a@gmail.com>
2018-11-02 12:40:09 -0400

    Pawn Placement

--- scripts/src/Pawn.js
+++ scripts/src/Pawn.js
@@ -5,5 +5,11 @@ var Pawn = function (game) {
 }
 
 Pawn.prototype.init = function init() {
-    pawn = this.game.add.isoSprite(0, 0, 0, 'boys', 'happy1-back');
+    this.pawn = this.game.add.isoSprite(108, 288, 0, 'boys', 'happy1-back');
+    this.pawn.anchor.set(0.5, 0.5);
+
+    this.pawn.animations.add('happy-walk', ["happy1", "happy2"], 2, true);
+    this.pawn.animations.add('happy-walk-back', ["happy1-back", "happy2-back"], 2, true);
+
+    this.pawn.animations.play('happy-walk');
 }
